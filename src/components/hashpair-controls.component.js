96080d888f1df111a0f7002d86094f75ab258f46
Owen Moore <hello@owenmoore.xyz>
2021-06-22 11:07:57 -0700

    hex values updating

--- src/components/hashpair-controls.component.js
+++ src/components/hashpair-controls.component.js
@@ -6,13 +6,15 @@ import HashPairSlider from "./hashpair-slider.component";
 const HashPairControls = (props) => {
     const [sliders, setSliders] = useState();
 
+    const { update } = props;
+
     useEffect(() => {
         let s = [];
         for (let i = 0; i < 32; i++) {
-            s = s.concat(<HashPairSlider key={i} index={i} />);
+            s = s.concat(<HashPairSlider key={i} index={i} update={update} />);
         }
         setSliders(s);
-    }, []);
+    }, [update]);
 
     return (
         <>
@@ -27,7 +29,11 @@ const HashPairControls = (props) => {
             <Segment inverted style={{ maxHeight: "calc(100vh - 240px)", overflow: "auto" }}>
                 <Segment.Group>{sliders}</Segment.Group>
             </Segment>
-            <Segment>Hash here</Segment>
+            <Segment>
+                <center>
+                    <span style={{ fontFamily: "monospace", fontSize: 12 }}>{props.hash}</span>
+                </center>
+            </Segment>
         </>
     );
 };
