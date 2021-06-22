fa09887e6f01b3add1fdf7c6bbfad905516da793
Owen Moore <hello@owenmoore.xyz>
2021-06-22 12:02:27 -0700

    plus minus buttons

--- src/components/hashpair-slider.component.js
+++ src/components/hashpair-slider.component.js
@@ -5,6 +5,7 @@ import { Slider } from "react-semantic-ui-range";
 
 const HashPairSlider = (props) => {
     const [hex, setHex] = useState("");
+    const [value, setValue] = useState(0);
 
     const { index, update } = props;
 
@@ -12,19 +13,34 @@ const HashPairSlider = (props) => {
         setHex("00");
     }, []);
 
-    function handleChange(value) {
-        var v = ValueToHexPair(value);
-        setHex(v);
-        update(index, v);
-    }
+    useEffect(() => {
+        var h = ValueToHexPair(value);
+        update(index, h);
+        if (hex !== h) setHex(h);
+    }, [value]);
+
+    const stepValue = (inc) => {
+        var v = value;
+        v += inc;
+        switch (v) {
+            case v >= 256:
+                v = 255;
+                break;
+            case v < 0:
+                v = 0;
+                break;
+            default:
+        }
+        setValue(v);
+    };
 
     const settings = {
         start: 0,
         min: 0,
         max: 255,
         step: 1,
-        onChange: (value) => {
-            handleChange(value);
+        onChange: (v) => {
+            setValue(v);
         },
     };
 
@@ -35,7 +51,14 @@ const HashPairSlider = (props) => {
                     <span style={{ top: 5, position: "relative" }}>{props.index}</span>
                 </Grid.Column>
                 <Grid.Column width={2}>
-                    <Button circular icon="minus" size="mini" />
+                    <Button
+                        circular
+                        icon="minus"
+                        size="mini"
+                        onClick={() => {
+                            stepValue(-1);
+                        }}
+                    />
                 </Grid.Column>
                 <Grid.Column width={8}>
                     <span style={{ top: 4, position: "relative" }}>
@@ -43,7 +66,14 @@ const HashPairSlider = (props) => {
                     </span>
                 </Grid.Column>
                 <Grid.Column width={2}>
-                    <Button circular icon="plus" size="mini" />
+                    <Button
+                        circular
+                        icon="plus"
+                        size="mini"
+                        onClick={() => {
+                            stepValue(1);
+                        }}
+                    />
                 </Grid.Column>
                 <Grid.Column width={3}>
                     <Input fluid maxLength="2" style={{ width: 50 }} value={hex} />
