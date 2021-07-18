7f2d29cb84b245e9c3f23c97da9070ec989b52ae
Grant Moore <grantmoore3d@gmail.com>
2021-07-17 22:46:07 -0700

    partially working set hash modal

--- src/components/modals/set-resolution.component.js
+++ src/components/modals/set-resolution.component.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Modal, Form, Message, Button } from "semantic-ui-react";
 
 const SetResolution = (props) => {
