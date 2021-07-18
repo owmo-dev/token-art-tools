e5789e6caefb866f7d50daf8e295c296e2a3c5c8
Owen Moore <hello@owenmoore.art>
2021-07-18 12:09:06 -0700

    enter hash string functional

--- src/containers/app.container.js
+++ src/containers/app.container.js
@@ -6,42 +6,50 @@ import "../css/style.css";
 import LeftControls from "../components/panels/left-controls.component";
 import MainViewer from "../components/panels/main-viewer.component";
 
+import { ValueToHexPair, HexPairToValue } from "../helpers/token.helpers";
+
 const App = () => {
     const [hash, setHash] = useState("0x0000000000000000000000000000000000000000000000000000000000000000");
-    const [hexValues, setHexValues] = useState(() => {
+
+    const [values, setValues] = useState(() => {
         var v = {};
         for (let i = 0; i < 32; i++) {
-            v[i] = "00";
+            v[i] = 0;
         }
         return v;
     });
 
-    const updateHexValue = (i, v) => {
-        setHexValues((prevState) => ({
-            ...prevState,
+    const setValueAtIndex = (i, v) => {
+        setValues((prev) => ({
+            ...prev,
             [i]: v,
         }));
     };
 
-    const enterHash = (h) => {
-        var hash = h.substring(2);
+    const setHashValues = (h) => {
+        h = h.substring(2);
         for (let i = 0; i < 32; i++) {
-            updateHexValue(i, hash[i * 2] + hash[i * 2 + 1]);
+            setValueAtIndex(i, HexPairToValue(h[i * 2] + h[i * 2 + 1]));
         }
     };
 
     useEffect(() => {
         var h = "0x";
         for (let i = 0; i < 32; i++) {
-            h += hexValues[i];
+            h += ValueToHexPair(values[i]);
         }
         if (hash !== h) setHash(h);
-    }, [hexValues, hash]);
+    }, [values, hash]);
 
     return (
         <div style={{ width: "100%", height: "100%", overflow: "hidden" }}>
             <div style={{ width: 480, position: "absolute", top: 20, left: 20 }}>
-                <LeftControls hash={hash} update={updateHexValue} setHash={enterHash} />
+                <LeftControls
+                    hash={hash}
+                    values={values}
+                    setValueAtIndex={setValueAtIndex}
+                    setHashValues={setHashValues}
+                />
             </div>
             <div
                 style={{
