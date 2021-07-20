9ee634fa9617231857fb61b91887e701d659eb23
Grant Moore <grantmoore3d@gmail.com>
2021-07-20 15:22:50 -0700

    automated screenshots working

--- src/components/modals/run-automation.component.js
+++ src/components/modals/run-automation.component.js
@@ -1,42 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { Modal, Button, Progress } from "semantic-ui-react";
+import React, { useEffect } from "react";
+import { Modal, Progress } from "semantic-ui-react";
 
 const RunAutomation = (props) => {
-    const { count, wait, close } = props;
-    const [current, setCurrent] = useState(0);
-    const [progress, setProgress] = useState(0);
-
-    function cancel() {
-        close();
-    }
+    const { progress, close } = props;
 
     useEffect(() => {
-        setProgress((current / count) * 100);
-        function automate() {
-            setTimeout(function () {
-                console.log("hello");
-                setCurrent(current + 1);
-                if (current < count) {
-                    automate();
-                } else {
-                    close();
-                }
-            }, wait);
-        }
-        automate();
-    }, []);
+        if (progress === 100)
+            setTimeout(() => {
+                close();
+            }, 1000);
+    }, [progress, close]);
 
     return (
         <Modal size="tiny" open={props.active}>
-            <Modal.Header>Generate Random Hash Images</Modal.Header>
+            <Modal.Header>Generating Images</Modal.Header>
             <Modal.Content>
                 <Progress percent={progress} indicating />
             </Modal.Content>
-            <Modal.Actions>
-                <Button secondary onClick={cancel}>
-                    Cancel
-                </Button>
-            </Modal.Actions>
         </Modal>
     );
 };
