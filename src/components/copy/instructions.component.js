5224f02254961a556a227821285b4ccf90ddac37
Grant Moore <grantmoore3d@gmail.com>
2021-07-28 10:47:56 -0700

    added instruction to use token hash

--- src/components/copy/instructions.component.js
+++ src/components/copy/instructions.component.js
@@ -4,16 +4,19 @@ import { Segment, Header, Icon, Input } from "semantic-ui-react";
 const Instructions = () => {
     const script =
         '<script src="https://cdn.jsdelivr.net/gh/ctrlshiftmake/token-art-tools/providers/artblocks.js"></script>';
+    const code = "var hash = tokenData.hash;";
     const localhost = "python -m http.server 5500";
     const localURL = "http://127.0.0.1:5500";
 
     return (
         <Segment
             style={{
-                width: "60%",
-                height: 350,
+                width: "70%",
+                minWidth: 450,
+                maxWidth: 800,
+                height: 440,
                 padding: 40,
-                top: "calc(50% - 175px)",
+                top: "calc(50% - 225px)",
                 margin: "auto",
                 boxShadow: "0 3px 3px rgba(0,0,0,0.2)",
             }}
@@ -33,6 +36,24 @@ const Instructions = () => {
                     },
                 }}
                 value={script}
+                readOnly={true}
+            />
+
+            <Header as="h4">
+                <Icon name="code" style={{ float: "left" }} />
+                Use the hash provided by the token in your script
+            </Header>
+
+            <Input
+                fluid
+                action={{
+                    color: "blue",
+                    icon: "copy",
+                    onClick: () => {
+                        navigator.clipboard.writeText(code);
+                    },
+                }}
+                value={code}
                 readOnly={true}
             />
 
