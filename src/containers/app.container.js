f7249aae700884810e3cf911de92fc4af074a95a
Owen Moore <hello@owenmoore.xyz>
2021-08-11 13:09:02 -0700

    slowed down some more, better results

--- src/containers/app.container.js
+++ src/containers/app.container.js
@@ -123,7 +123,7 @@ const App = () => {
                 var f = features;
                 f["Hash"] = hash;
                 setFeaturesList((prev) => [...prev, f]);
-            }, 800);
+            }, 1000);
         }
         setProgress(parseInt((state / total) * 100));
         if (state === total) return;
