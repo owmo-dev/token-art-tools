4f2025c34a3fada6d3db3668e7a01e973750ebf2
Owen Moore <hello@owenmoore.art>
2021-11-06 15:29:52 -0700

    Updates

---
+++ component---src-pages-404-js-36a7d92220fa7b4634b0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktoken_art_tools=self.webpackChunktoken_art_tools||[]).push([[883],{9616:function(t,e,n){n.r(e);var u=n(7294);e.default=function(){return u.createElement("h1",null,"404")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-36a7d92220fa7b4634b0.js.map
--- component---src-pages-404-js-61c74fc5473b5949dfdf.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunktoken_art_tools=self.webpackChunktoken_art_tools||[]).push([[883],{9616:function(t,e,n){"use strict";n.r(e);var u=n(7294);e.default=function(){return u.createElement("h1",null,"404")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-61c74fc5473b5949dfdf.js.map
