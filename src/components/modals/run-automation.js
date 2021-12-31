eb30205f2c69d4feb1136cf45c2896dcea128927
Owen Moore <hello@owenmoore.art>
2021-12-31 14:19:54 -0800

    automation functional again, CSV needs implementation

--- src/components/modals/run-automation.js
+++ src/components/modals/run-automation.js
@@ -1,9 +1,13 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Modal, Progress, Icon} from 'semantic-ui-react';
 
+import {useHash} from '../../hooks/useHash';
 import {useAutomation} from '../../hooks/useAutomation';
 
+import {screenshot} from '../../helpers/screenshot';
+
 const RunAutomation = () => {
+    const [hash, hashAction] = useHash();
     const [automation, automationAction] = useAutomation();
 
     const msg_gen = 'Generating Hashes';
@@ -12,26 +16,64 @@ const RunAutomation = () => {
     const [isSubmitting, setSubmitState] = useState(false);
     const [message, setMessage] = useState(msg_gen);
 
-    /*
+    const [runner, setRunner] = useState(null);
+
     useEffect(() => {
-        if (active) {
-            setMessage(msg_gen);
+        if (automation.status === 'active' && runner === null) {
+            hashAction({type: 'random'});
+            setRunner(
+                setInterval(() => {
+                    automationAction({
+                        type: 'tick',
+                    });
+                }, automation.waitTime),
+            );
+        }
+    }, [automation.status]);
+
+    useEffect(() => {
+        if (automation.status === 'idle' && isSubmitting === true) {
             setSubmitState(false);
         }
-    }, [active]);
-    
+    }, [automation.status]);
+
     useEffect(() => {
-        if (progress === 100) {
-            setMessage(msg_fin);
-            setTimeout(() => {
-                close();
-            }, 2000);
+        if (automation.status === 'active') {
+            if (automation.tick > 0 && automation.tick <= automation.total) {
+                if (message !== msg_gen) setMessage(msg_gen);
+
+                if (automation.doScreenshot) {
+                    screenshot(hash.hash);
+                }
+
+                // if doCSVExport, capture features.... !!!
+            }
+
+            if (automation.tick === automation.total) {
+                automationAction({type: 'stop'});
+            } else {
+                hashAction({type: 'random'});
+            }
         }
-    }, [progress, close]);
-    */
+    }, [automation.status, automation.tick]);
+
+    useEffect(() => {
+        if (automation.status === 'stopping') {
+            clearInterval(runner);
+            setRunner(null);
+
+            if (message !== msg_fin) setMessage(msg_fin);
+
+            // if doCSVExport, wait and gather features into CSV
+
+            setTimeout(() => {
+                automationAction({type: 'reset'});
+            }, 500);
+        }
+    }, [automation.status]);
 
     return (
-        <Modal size="tiny" open={automation.status === 'active'}>
+        <Modal size="tiny" open={automation.status === 'active' || automation.status === 'stopping'}>
             <Modal.Header>Running Automation</Modal.Header>
             <Modal.Content>
                 <Progress percent={automation.progress} progress indicating>
